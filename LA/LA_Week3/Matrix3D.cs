0cd4e174f12eec36e0e373ebf4a177a72ecd3ef6
Niek Willems <niek.willems91@gmail.com>
2014-11-30 10:31:59 +0100

    Matrix3d

---
+++ LA/LA_Week3/Matrix3D.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LA_Week3
+{
+    public class Matrix3D : Matrix
+    {
+        public static Matrix Scale( double x, double y, double z)
+        {
+
+        }
+
+        public static Matrix Translate( double x, double y, double z)
+        {
+
+        }
+
+        protected static Matrix RotateOverX( double alpha )
+        {
+
+        }
+
+        protected static Matrix RotateOverY( double alpha )
+        {
+
+        }
+
+        protected static Matrix RotateOverZ( double alpha )
+        {
+
+        }
+    }
+}
